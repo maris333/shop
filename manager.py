6213cec960e1a54c74315b2eb925de387a83a714
maris333 <animaral@gmail.com>
2023-04-30 17:38:53 +0200

    Update test.py
    Update test.py
    
    Update test.py
    
    Update

--- manager.py
+++ manager.py
@@ -4,39 +4,51 @@ from order import Order
 
 
 class Manager:
-    def __init__(self, quantity_in_stock: Dict[str, int]):
-        self.quantity_in_stock = quantity_in_stock
+    def __init__(self, quantity_in_stock: Dict[Order, int]):
+        self.orders_in_stock = quantity_in_stock
         self.orders: Dict[Order, int] = {}
 
-    def check_if_order_is_in_stock(self, order: Order, amount: int):
-        if order.name in self.quantity_in_stock:
-            if self.quantity_in_stock[order.name] >= amount:
+    def get_order_in_stock_by_id(self, id: int):
+        for order_in_stock in self.orders_in_stock.keys():
+            if order_in_stock.id == id:
+                return order_in_stock
+
+    def check_if_order_is_in_stock(self, order: Order, amount: int, id: int):
+        if self.get_order_in_stock_by_id(id) is not None:
+            order_in_stock = self.get_order_in_stock_by_id(id)
+            if self.orders_in_stock[order_in_stock] >= amount:
                 return True
-            print("There is not enough {} in stock".format(order.name))
+            print("There is not enough {} in stock".format(order_in_stock.name))
             return False
         print("{} are not in stock".format(order.name.title()))
         return False
 
-    def add_order(self, order: Order, amount: int):
-        if self.check_if_order_is_in_stock(order, amount):
+    def add_order(self, order: Order, amount: int, id: int):
+        if self.check_if_order_is_in_stock(order, amount, id):
+            order_in_stock = self.get_order_in_stock_by_id(id)
             if order in self.orders:
                 self.orders[order] += amount
-                self.quantity_in_stock[order.name] -= amount
+                self.orders_in_stock[order_in_stock] -= amount
+                print("{} of {} was added".format(amount, order.name))
                 return True
             self.orders[order] = amount
-            self.quantity_in_stock[order.name] -= amount
+            self.orders_in_stock[order_in_stock] -= amount
+            print("{} of {} was added".format(amount, order.name))
             return True
         return False
 
-    def delete_order(self, order: Order, amount: int):
+    def delete_order(self, order: Order, amount: int, id: int):
         if order in self.orders:
+            order_in_stock = self.get_order_in_stock_by_id(id)
             if self.orders[order] == amount:
                 self.orders.pop(order)
-                self.quantity_in_stock[order.name] += amount
+                self.orders_in_stock[order_in_stock] += amount
+                print("{} of {} were deleted".format(amount, order.name))
                 return True
             elif self.orders[order] > amount:
                 self.orders[order] -= amount
-                self.quantity_in_stock[order.name] += amount
+                self.orders_in_stock[order_in_stock] += amount
+                print("{} of {} were deleted".format(amount, order.name))
                 return True
             print("The quantity to be removed exceeds the order")
             return False
--- test.py
+++ test.py
@@ -1,35 +1,84 @@
-from unittest import TestCase
+from decimal import Decimal
 
 from manager import Manager
 from order import Order
 
 
-class Test(TestCase):
-    # only work when run separately ;/
+class Test:
+    def test_get_order_in_stock_by_id(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        assert manager.get_order_in_stock_by_id(order.id).id, order.id
 
-    def testAddOrder(self):
-        order = Order(1, "apples", 10)
-        manager = Manager()
-        manager.add_order(order)
-        self.assertEqual(len(manager.orders), 1)
+    def test_check_if_order_is_in_stock(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        assert manager.check_if_order_is_in_stock(order, 10, 1)
 
-    def testAddExistingOrder(self):
-        order = Order(1, "apples", 10)
-        manager = Manager()
-        manager.add_order(order)
-        manager.add_order(order)
-        self.assertEqual(manager.orders[order], 2)
+    def test_check_if_order_is_not_in_stock(self):
+        order = Order(3, "grapes", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        assert not manager.check_if_order_is_in_stock(order, 10, 3)
 
-    def testDeleteOrder(self):
-        manager = Manager()
-        order = Order(1, "apples", 10)
+    def test_check_if_too_little_is_in_stock(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        assert not manager.check_if_order_is_in_stock(order, 110, 1)
+
+    def test_add_order(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(2, "bananas", Decimal(10)): 100})
+        manager.add_order(order, 10, 1)
+        assert len(manager.orders) == 1
+
+    def test_add_existing_order(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(2, "bananas", Decimal(10)): 100})
+        manager.add_order(order, 1, 1)
+        manager.add_order(order, 1, 1)
+        assert manager.orders[order], 2
+
+    def test_add_existing_order_check_if_stock_is_reduced(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        manager.add_order(order, 10, 1)
+        order_in_stock = manager.get_order_in_stock_by_id(order.id)
+        assert manager.orders_in_stock[order_in_stock], 90
+
+    def test_add_existing_order_check_if_not_in_stock(self):
+        order = Order(3, "apples", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        assert not manager.add_order(order, 10, 3)
+
+    def test_add_existing_order_check_if_not_enough_in_stock(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        assert not manager.add_order(order, 110, 1)
+
+    def test_delete_order(self):
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        order = Order(1, "oranges", Decimal(10))
         manager.orders = {order: 1}
-        manager.delete_order(order)
-        self.assertEqual(len(manager.orders), 0)
+        manager.delete_order(order, 1, 1)
+        assert len(manager.orders) == 0
 
-    def testDeleteExistingOrder(self):
-        manager = Manager()
-        order = Order(1, "apples", 10)
-        manager.orders = {order: 2}
-        manager.delete_order(order)
-        self.assertEqual(manager.orders[order], 1)
+    def test_delete_more_than_one_order(self):
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        order = Order(1, "oranges", Decimal(10))
+        manager.orders = {order: 3}
+        manager.delete_order(order, 2, 1)
+        assert manager.orders[order], 1
+
+    def test_delete_not_existing_order(self):
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        order = Order(3, "apples", Decimal(10))
+        assert not manager.delete_order(order, 1, 3)
+
+    def test_delete_existing_order_check_if_stock_is_refilled(self):
+        order = Order(1, "oranges", Decimal(10))
+        manager = Manager({Order(1, "oranges", Decimal(10)): 100, Order(1, "bananas", Decimal(10)): 100})
+        manager.add_order(order, 10, 1)
+        manager.delete_order(order, 10, 1)
+        order_in_stock = manager.get_order_in_stock_by_id(order.id)
+        assert manager.orders_in_stock[order_in_stock], 100
+
