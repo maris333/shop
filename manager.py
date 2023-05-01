3fc2bc05d2a2a37c8a65e86b8b36d6f1532297bc
maris333 <animaral@gmail.com>
2023-05-01 16:12:38 +0200

    Update manager.py

--- manager.py
+++ manager.py
@@ -4,8 +4,8 @@ from order import Order
 
 
 class Manager:
-    def __init__(self, quantity_in_stock: Dict[Order, int]):
-        self.orders_in_stock = quantity_in_stock
+    def __init__(self, orders_in_stock: Dict[Order, int]):
+        self.orders_in_stock = orders_in_stock
         self.orders: Dict[Order, int] = {}
 
     def get_order_in_stock_by_id(self, id: int):
@@ -13,9 +13,9 @@ class Manager:
             if order_in_stock.id == id:
                 return order_in_stock
 
-    def check_if_order_is_in_stock(self, order: Order, amount: int, id: int):
-        if self.get_order_in_stock_by_id(id) is not None:
-            order_in_stock = self.get_order_in_stock_by_id(id)
+    def check_if_order_is_in_stock(self, order: Order, amount: int):
+        if self.get_order_in_stock_by_id(order.id) is not None:
+            order_in_stock = self.get_order_in_stock_by_id(order.id)
             if self.orders_in_stock[order_in_stock] >= amount:
                 return True
             print("There is not enough {} in stock".format(order_in_stock.name))
@@ -23,9 +23,9 @@ class Manager:
         print("{} are not in stock".format(order.name.title()))
         return False
 
-    def add_order(self, order: Order, amount: int, id: int):
-        if self.check_if_order_is_in_stock(order, amount, id):
-            order_in_stock = self.get_order_in_stock_by_id(id)
+    def add_order(self, order: Order, amount: int):
+        if self.check_if_order_is_in_stock(order, amount):
+            order_in_stock = self.get_order_in_stock_by_id(order.id)
             if order in self.orders:
                 self.orders[order] += amount
                 self.orders_in_stock[order_in_stock] -= amount
@@ -37,9 +37,9 @@ class Manager:
             return True
         return False
 
-    def delete_order(self, order: Order, amount: int, id: int):
+    def delete_order(self, order: Order, amount: int):
         if order in self.orders:
-            order_in_stock = self.get_order_in_stock_by_id(id)
+            order_in_stock = self.get_order_in_stock_by_id(order.id)
             if self.orders[order] == amount:
                 self.orders.pop(order)
                 self.orders_in_stock[order_in_stock] += amount
