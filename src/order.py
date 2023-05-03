af992e477e2031b7784302db684b6797a7d78c28
maris333 <animaral@gmail.com>
2023-05-03 08:22:25 +0200

    Fixes for lint

--- src/order.py
+++ src/order.py
@@ -12,9 +12,9 @@ class Order:
 
     def __eq__(self, other):
         if isinstance(other, Order):
-            return(self.id, self.name, self.price) == (
+            return (self.id, self.name, self.price) == (
                 other.id,
                 other.name,
-                other.price
+                other.price,
             )
         return False
