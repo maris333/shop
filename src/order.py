f6e6d286fa4484883c41feb31641e420219326ac
maris333 <animaral@gmail.com>
2023-05-03 07:44:39 +0200

    Add __hash__ and __eq__ methods to order.py

--- src/order.py
+++ src/order.py
@@ -6,3 +6,11 @@ class Order:
         self.id = id
         self.name = name
         self.price = price
+
+    def __hash__(self):
+        return hash((self.id, self.name, self.price))
+
+    def __eq__(self, other):
+        if isinstance(other, Order):
+            return(self.id, self.name, self.price) == (other.id, other.name, other.price)
+        return False
